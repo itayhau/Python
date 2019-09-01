510df0f78087218a1c2797c3452060de948b6e86
itayhau <itayhau@gmail.com>
2019-09-01 12:14:44 +0300

    Update TKinter.py

--- TKinter.py
+++ TKinter.py
@@ -32,6 +32,20 @@ print('Beginning file download with urllib2...')
 
 url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
 urllib.request.urlretrieve(url, 'c:/temp/cat.jpg')
+
+OR:
+
+import requests
+import os
+
+url = 'https://apod.nasa.gov/apod/image/1701/potw1636aN159_HST_2048.jpg'
+page = requests.get(url)
+
+f_ext = os.path.splitext(url)[-1]
+f_name = 'img{}'.format(f_ext)
+with open(f_name, 'wb') as f:
+    f.write(page.content)
+    
 '''
 
 '''
